26820816d555618861a6133aba370842e082ba04
Renato <36175588+renatoalvesjr@users.noreply.github.com>
2025-04-30 10:23:37 -0300

    add key button feature

--- src/app/components/translation-table/translation-table.component.ts
+++ src/app/components/translation-table/translation-table.component.ts
@@ -13,11 +13,33 @@ import {FormsModule} from '@angular/forms';
 export class TranslationTableComponent {
   @Input() translation!: { [lang: string]: any };
 
+  newKey = '';
+
+  set keys(key: string) {
+    this.keys.push(key);
+  }
+
   get keys(): string[] {
     return Object.keys(this.translation);
   }
 
   get languages(): string[] {
     return Object.keys(this.translation[this.keys[0]]);
+  }
+
+  set languages(language: string) {
+    this.languages.push(language);
+  }
+
+  addKey() {
+    if (this.newKey) {
+      for (const key of this.keys) {
+        this.translation[key] ??= {};
+        this.translation[key][this.newKey] ??= '';
+      }
+      this.newKey = '';
+      // Trigger UI update; reassigning the translation object to itself
+      this.translation = { ...this.translation };
+    }
   }
 }
